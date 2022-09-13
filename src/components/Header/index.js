b35e990d29083c2affcb64a69419d2cd49bf57fb
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-09-12 20:54:29 -0500

    Completed skills section

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAddressCard, faHouse, faDiagramProject, faFile } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-scroll";
+import {
+  faAddressCard,
+  faHouse,
+  faDiagramProject,
+  faFile,
+  faArrowCircleDown,
+} from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import "./header.css";
 
@@ -10,30 +17,21 @@ export default function Header() {
     <div className="header">
       <Navbar bg="dark" variant="dark" fixed="top" expand="xxl">
         <Navbar.Brand>
-          <a href="https://github.com/Castoreno05/Project_3">
-          </a>
+          <a href="https://github.com/Castoreno05/Project_3"></a>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav>
-            <Nav.Link
-              href="/"
-            >
+            <Nav.Link href="/">
               <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon>
             </Nav.Link>
-            <Nav.Link
-              href="/projects"
-            >
+            <Nav.Link href="/projects">
               <FontAwesomeIcon icon={faDiagramProject}></FontAwesomeIcon>
             </Nav.Link>
-            <Nav.Link
-              href="/about"
-            >
+            <Nav.Link href="/about">
               <FontAwesomeIcon icon={faAddressCard}></FontAwesomeIcon>
             </Nav.Link>
-            <Nav.Link
-              href="/#skills"
-            >
+            <Nav.Link href="/#skills">
               <FontAwesomeIcon icon={faFile}></FontAwesomeIcon>
             </Nav.Link>
           </Nav>
@@ -42,15 +40,29 @@ export default function Header() {
       <section className="intro">
         <h1 className="name">Matthew Castoreno</h1>
         <div className="paragraphDiv">
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."</p>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur."
+          </p>
         </div>
         <div className="socialLinks">
-        <FontAwesomeIcon id="githubLink" icon={faEnvelope}></FontAwesomeIcon>
-        <FontAwesomeIcon id="resumeLink" icon={faEnvelope}></FontAwesomeIcon>
-        <FontAwesomeIcon id="linkedinLink" icon={faEnvelope}></FontAwesomeIcon>
+          <FontAwesomeIcon id="githubLink" icon={faEnvelope}></FontAwesomeIcon>
+          <FontAwesomeIcon id="resumeLink" icon={faEnvelope}></FontAwesomeIcon>
+          <FontAwesomeIcon
+            id="linkedinLink"
+            icon={faEnvelope}
+          ></FontAwesomeIcon>
         </div>
       </section>
+      <div className="arrowDown">
+        <Link to="about" spy={true} smooth={true} duration={500}>
+        <FontAwesomeIcon icon={faArrowCircleDown}></FontAwesomeIcon>
+        </Link>
+      </div>
     </div>
   );
 }
-
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -8,14 +8,14 @@ export default function Skills() {
         <h3 className="skillTitle">Education</h3>
         <div className="education-columns">
           <h2>University of Texas - Austin, Texas</h2>
+          <p className="info"><em>Full-stack web development</em> <span>•</span> 2022</p>
           <div className="skillColumn">
             Ei omittam disputationi concludaturque per, cum ex labores appareat.
-            Eam maiestatis dissentiunt ut. At mel altera aperiri mandamus. Eam
-            offendit scripserit te.Ei omittam disputationi concludaturque per,
-            cum ex labores appareat. Eam maiestatis dissentiunt ut. At mel
+            Eam maiestatis dissentiunt ut. At mel altera aperiri mandamus.At mel
             altera aperiri mandamus. Eam offendit scripserit te.
           </div>
           <h2>San Jacinto College - Pasadena, Texas</h2>
+          <p className="info"><em>Process Technology</em> <span>•</span> 2016</p>
           <div className="skillColumn">
             Ei omittam disputationi concludaturque per, cum ex labores appareat.
             Eam maiestatis dissentiunt ut. At mel altera aperiri mandamus. Eam
@@ -29,6 +29,7 @@ export default function Skills() {
         <h3 className="skillTitle">Work</h3>
         <div className="work-columns">
           <h2>Eurecat US Inc</h2>
+          <p className="info"><em>Process Operator</em> <span>•</span> 2022</p>
           <div className="skillColumn">
             Ei omittam disputationi concludaturque per, cum ex labores appareat.
             Eam maiestatis dissentiunt ut. At mel altera aperiri mandamus. Eam
@@ -37,6 +38,7 @@ export default function Skills() {
             altera aperiri mandamus. Eam offendit scripserit te.
           </div>
           <h2>The Lubrizol Corporation</h2>
+          <p className="info"><em>Process Operator</em> <span>•</span> 2016-2020</p>
           <div className="skillColumn">
             Ei omittam disputationi concludaturque per, cum ex labores appareat.
             Eam maiestatis dissentiunt ut. At mel altera aperiri mandamus. Eam
@@ -55,11 +57,23 @@ export default function Skills() {
             offendit scripserit te.Ei omittam disputationi concludaturque per,
             cum ex labores appareat. Eam maiestatis dissentiunt ut. At mel
             altera aperiri mandamus. Eam offendit scripserit Ei omittam
-            disputationi concludaturque per, cum ex labores appareat. Eam
-            maiestatis dissentiunt ut. At mel altera aperiri mandamus. Eam
-            offendit scripserit te.Ei omittam disputationi concludaturque per,
-            cum ex labores appareat. Eam maiestatis dissentiunt ut. At mel
-            altera aperiri mandamus. Eam offendit scripserit te.
+            disputationi concludaturque per, cum ex labores appareat.
+          </div>
+          <p>HTML</p>
+          <div className="strength-container">
+            <div className="hyper"></div>
+          </div>
+          <p>CSS</p>
+          <div className="strength-container">
+            <div className="css"></div>
+          </div>
+          <p>Javascript</p>
+          <div className="strength-container">
+            <div className="java"></div>
+          </div>
+          <p>MySQL</p>
+          <div className="strength-container">
+            <div className="mySQL"></div>
           </div>
         </div>
       </div>
