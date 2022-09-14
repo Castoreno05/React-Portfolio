8ce2c7a6c528c3df5a6dc5d81034a8bd3f561493
Matthew <castoreno_05@Yessicas-MacBook-Pro.local>
2022-09-14 18:24:52 -0500

    Added github pages

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -35,14 +35,14 @@ export default function Contact() {
 
   return (
     <div className="contactForm">
-      <div class="contactContainer">
+      <div className="contactContainer">
         <div id="contact" action="" method="post">
           <h3>Contact me</h3>
           <br></br>
           <input
             placeholder="Your name"
             type="text"
-            tabindex="1"
+            tabIndex="1"
             required
             onChange={(event) => {
               setTo_name(event.target.value);
@@ -51,7 +51,7 @@ export default function Contact() {
           <input
             placeholder="Your Email Address"
             type="email"
-            tabindex="2"
+            tabIndex="2"
             required
             onChange={(event) => {
               setFrom_name(event.target.value);
@@ -59,7 +59,7 @@ export default function Contact() {
           />
           <textarea
             placeholder="Type your message here...."
-            tabindex="3"
+            tabIndex="3"
             required
             onChange={(event) => {
               setMessage(event.target.value);
@@ -68,7 +68,7 @@ export default function Contact() {
           <button name="submit" type="submit" onClick={submitInfo}>
             Submit
           </button>
-          <p class="copyright">
+          <p className="copyright">
             Github Username{" "}
             <a
               href="https://github.com/Castoreno05"
