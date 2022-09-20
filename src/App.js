4fa5bc414c90a21bd4d2dbc47f8e966431ede4ce
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-09-20 08:08:10 -0500

    Removed ContactForm index.js from components. Then removed render on app.js. Contact.css is for Modal.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-// import Contact from "./components/ContactForm";
 import Footer from "./components/Footer";
 import "./app.css";
 
-
 function App() {
-  // const [content, setContent] = useState("about-me");
 
   return (
     <div className="App">
@@ -17,7 +14,6 @@ function App() {
       <About/>
       <Skills/>
       <Projects/>
-      {/* <Contact /> */}
       <Footer/>
     </div>
   );
