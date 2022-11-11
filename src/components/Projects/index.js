27675668179a4dea5f7687378f7664cb68ca6d1b
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-11-11 14:07:08 -0600

    Removed and updated Collapsible

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,130 +1,66 @@
-import React from "react";
-import Collapsible from "../Collapsible";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import { Link } from "react-scroll";
-import "./projects.css";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { projectData } from "./projectData";
 
 export default function Projects() {
+  const [selected, setSelected] = useState(null);
+
+  const toggle = (i) => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+
+    setSelected(i);
+  };
+
   return (
-    <div className="projects" id="projects">
-      <h1>Applications</h1>
-      <Container className="projectContainer">
-      <Link
-          to="projectOne"
-          spy={true}
-          smooth={true}
-          offset={-650}
-          duration={50}
-        >
-        <Row className="projectRow">
-          <Collapsible label="Bootcamp Blog">
-            <div className="imageOne"></div>
-            <p id="projectOne">
-              The Bootcamp Blog is a MERN stack application that allows users to
-              sign-up/log-in and post blogs for other users to read and comment
-              on. Create posts in the dashboard and save them to your profile.
-              If edits of the post need to be made, the options to delete or
-              update are given. When a user submits a post to the homepage, any
-              user <strong>currently</strong> signed in will have the
-              oppoptunity to leave comments.
-            </p>
-          </Collapsible>
-        </Row>
-        </Link>
-        <Link
-          to="projectTwo"
-          spy={true}
-          smooth={true}
-          offset={-650}
-          duration={50}
-        >
-          <Row className="projectRow">
-            <Collapsible label="Weather App">
-              <div className="imageTwo"></div>
-              <p id="projectTwo">
-                Created a Weather Application with the help of a third party API
-                service. Enter a city name in the search bar and the application
-                will make an API call to retrieve, then append live data onto
-                the home page. The data consists of: tempurature, wind speed,
-                humidity, and UV index for the current and five day forecast.
-                Once a search has been made the application saves the city in
-                local strorage. Doing this allows the user to generate another
-                API call to get live results of the weather.
-              </p>
-            </Collapsible>
-          </Row>
-        </Link>
-        <Link
-          to="projectThree"
-          spy={true}
-          smooth={true}
-          offset={-650}
-          duration={50}
-        >
-          <Row className="projectRow">
-            <Collapsible label="Professional README Generator">
-              <div className="imageThree"></div>
-              <p id="projectThree">
-                Utilize this command-line application to build a professional
-                README.md for current or future projects. Answer the questions
-                and give a brief description of the topics after the application
-                has been installed and started in the integrated terminal. When
-                the questions are finished the application will generate and
-                store a README.md inside the Develop folder.
-              </p>
-            </Collapsible>
-          </Row>
-        </Link>
-        <Link
-          to="projectFour"
-          spy={true}
-          smooth={true}
-          offset={-650}
-          duration={50}
-        >
-          <Row className="projectRow">
-            <Collapsible label="Workday Planner">
-              <div className="imageFour"></div>
-              <p id="projectFour">
-                Download this application to start tracking and planning your
-                workday tasks. When the repository has been cloned, open the
-                application within the browser. This application uses local date
-                and time to display to the user the past, present, and future
-                hours in a workday. Inside the time blocks, add a task and
-                select the blue save button. The application will then save the
-                task to local storage for future reference.
-              </p>
-            </Collapsible>
-          </Row>
-        </Link>
-        <Link
-          to="projectFive"
-          spy={true}
-          smooth={true}
-          offset={-650}
-          duration={50}
-        >
-          <Row className="projectRow">
-            <Collapsible label="codeQuiz">
-              <div className="imageFive"></div>
-              <p id="projectFive">
-                Press play! This module will ask you 5 questions that will put
-                your JavaScript, Developer Tools, and basic coding skills to the
-                test. Each question is worth 20 points, so try to not miss! With
-                every question that is missed, the timer drops 10 seconds,
-                increasing the pressure on the player. When the timer runs out
-                the game is over and the score that you ended with will display
-                on the screen. A "Save initals" and "Input Field" will display.
-                Save your initials, reset and play again!
-              </p>
-            </Collapsible>
-          </Row>
-        </Link>
-      </Container>
-      <br></br>
-      <br></br>
-      <br></br>
-    </div>
+    <Container>
+      {projectData.map((item, i) => (
+        <div className="item">
+          <div className="title" onClick={() => toggle(i)}>
+            <h2>{item.title}</h2>
+            <span>{selected === i ? "-" : "+"}</span>
+          </div>
+          <div className={selected === i ? "show" : "content"}>
+            <section>{item.video}</section>
+            <p>{item.description}</p>
+          </div>
+        </div>
+      ))}
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1.2rem;
+  gap: 1rem;
+  .item {
+    cursor: pointer;
+    background-color: whitesmoke;
+    width: 50vw;
+    .title {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .content {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+      justify-content: center;
+      overflow: hidden;
+      max-height: 0;
+      transition: all 0.5s cubic-bezier(0, 1, 0, 1);
+    }
+    .show {
+      height: auto;
+      max-height: 9999;
+      transition: all 0.5s cubic-bezier(1, 0, 1, 0);
+    }
+  }
+`;
---
+++ src/components/Projects/projectData.js
@@ -0,0 +1,37 @@
+export const projectData = [
+  {
+    id: 1,
+    title: "BootCamp Blog",
+    video: <img src={require('../../Videos/bootcampBlog.gif')} alt="camp-gif"/>,
+    description:
+      "Created a Weather Application with the help of a third party API service. Enter a city name in the search bar and the application will make an API call to retrieve, then append live data onto the home page. The data consists of: tempurature, wind speed, humidity, and UV index for the current and five day forecast. Once a search has been made the application saves the city in local strorage. Doing this allows the user to generate another API call to get live results of the weather.",
+  },
+  {
+    id: 2,
+    title: "WeatherApp",
+    video: <img src={require('../../Videos/WeatherGif.gif')} alt="weather-gif"/>,
+    description:
+      "Created a Weather Application with the help of a third party API service. Enter a city name in the search bar and the application will make an API call to retrieve, then append live data onto the home page. The data consists of: tempurature, wind speed, humidity, and UV index for the current and five day forecast. Once a search has been made the application saves the city in local strorage. Doing this allows the user to generate another API call to get live results of the weather.",
+  },
+  {
+    id: 3,
+    title: "Professional README Generator",
+    video: <img src={require('../../Videos/readmeGenerator.gif')} alt="generator-gif"/>,
+    description:
+      "Created a Weather Application with the help of a third party API service. Enter a city name in the search bar and the application will make an API call to retrieve, then append live data onto the home page. The data consists of: tempurature, wind speed, humidity, and UV index for the current and five day forecast. Once a search has been made the application saves the city in local strorage. Doing this allows the user to generate another API call to get live results of the weather.",
+  },
+  {
+    id: 4,
+    title: "Work Day Planner",
+    video: <img src={require('../../Videos/workdayPlanner.gif')} alt="planner-gif"/>,
+    description:
+      "Created a Weather Application with the help of a third party API service. Enter a city name in the search bar and the application will make an API call to retrieve, then append live data onto the home page. The data consists of: tempurature, wind speed, humidity, and UV index for the current and five day forecast. Once a search has been made the application saves the city in local strorage. Doing this allows the user to generate another API call to get live results of the weather.",
+  },
+  {
+    id: 5,
+    title: "codeQuiz",
+    video: <img src={require('../../Videos/codeQuiz.gif')} alt="quiz-gif"/>,
+    description:
+      "Created a Weather Application with the help of a third party API service. Enter a city name in the search bar and the application will make an API call to retrieve, then append live data onto the home page. The data consists of: tempurature, wind speed, humidity, and UV index for the current and five day forecast. Once a search has been made the application saves the city in local strorage. Doing this allows the user to generate another API call to get live results of the weather.",
+  },
+];
